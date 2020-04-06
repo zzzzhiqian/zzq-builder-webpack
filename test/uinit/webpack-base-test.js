55c9dd8a81edc29dd519ca6fa598b05745848715
zzzzhiqian <1125159606@qq.com>
2020-04-06 21:15:15 +0800

    test: webpack.base test

--- test/uinit/webpack-base-test.js
+++ test/uinit/webpack-base-test.js
@@ -5,7 +5,7 @@ describe('webpack.base.js test case', () => {
   const baseConfing = require('../../lib/webpack.base.js')
   console.log(baseConfing)
   it('entry', () => {
-    assert.equal(baseConfing.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js') > -1, true)
-    assert.equal(baseConfing.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js') > -1, true)
+    assert.equal(baseConfing.entry.index.indexOf('test/smoke/template/src/index/index.js') > -1, true)
+    assert.equal(baseConfing.entry.search.indexOf('test/smoke/template/src/search/index.js') > -1, true)
   })
 })
